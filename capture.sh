066efc74234d399aac41a30c505f403ba0071af4
Jiwoo Hong <93971542+hjiwoo27@users.noreply.github.com>
2023-12-06 19:16:04 +0900

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -9,23 +9,20 @@ clean_exit() {
 }
 
 # wav파일(random voice) & output pcap 파일이 저장될 directory 설정 
-folder_path="/path/to/your/wav/folder"
-output_path="/home/user/captured_traffic/"
+folder_path="/home/aisec/Desktop/Zoom_fingerprinting--main/random_voice/"
+output_path="/home/aisec/Desktop/Zoom_fingerprinting--main/output_files/"
 
 # 현재 시간을 가져와서 output file 이름으로 설정 
 current_time=$(date +'%Y-%m-%d_%H-%M-%S')
-
-output_file="${output_path}captured_traffic_${current_time}.pcap"
 
 # 캡쳐할 시간 설정 (초 단위)
 capture_duration=300  # 5분 = 300초
 
 # 파일명에 시간을 포함하여 설정
-output_file="captured_traffic_${current_time}.pcap"
+output_file="${output_path}captured_traffic_${current_time}.pcap"
 
 # tcpdump 실행하여 트래픽 캡쳐
-sudo tcpdump -U -i wlan0 -w $output_file -G $capture_duration -Z root
-
+sudo tcpdump -U -i wlan0 -w $output_file -G $capture_duration -Z root &
 
 # 시작 시간 기록
 start_time=$(date +%s)
@@ -50,3 +47,6 @@ while true; do
     echo "다음 WAV 파일까지 대기 시간: $random_sleep 초"
     sleep $random_sleep
 done
+
+sudo pkill -2 tcpdump
+clean_exit
